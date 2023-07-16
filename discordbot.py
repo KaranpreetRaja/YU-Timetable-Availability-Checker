c0a316671dead02d403c6917aad276ca2fdd3b81
Karanpreet Raja <86526687+KaranpreetRaja@users.noreply.github.com>
2023-07-15 22:17:42 -0400

    Creates Course Class

--- discordbot.py
+++ discordbot.py
@@ -5,38 +5,61 @@ from dotenv import load_dotenv
 from discord.ext import commands
 import pickle
 
+
+class Course:
+    def __init__(self, Faculty, Subject, Session, CourseCode, Section):
+        self.faculty = Faculty
+        self.subject = Subject
+        self.session = Session
+        self.courseCode = CourseCode
+        self.section = Section
+        self.subscribers = []
+
+    def subscribe(self, userID):
+        self.subscribers.append(userID)
+
+    def unsubscribe(self, userID):
+        self.subscribers.remove(userID)
+
+
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
     print("Bot is ready")
 
+
 @client.event
 async def on_message(message):
-    print("Message recieved by bot")
+    print("Message received by bot")
     if message.author == client.user:
         return
+
+    if message.content.startswith('!lastAvailable'):
+        # get the parameter after the command
+        pattern = r'!lastAvailable\s+(.+)'
+        match = re.search(pattern, message.content)
+
+        # get the course code
+        if match:
+            courseCode = match.group(1)
+            print(f"Course code: {courseCode}")
 
     if message.content.startswith('!hello'):
         await message.channel.send("Hello!")
 
-    if message.content.startswith('!addWaitlist'):
-        pattern = r'!command\s+(.+)'
+    if message.content.startswith('!courseSub'):
+        pattern = r'!courseSub\s+(.+)\s+(.+)'
         match = re.search(pattern, message.content)
 
         if match:
-            course = match.group(1)
+            course, section = match.group(1)
             user = message.author.id
-            print(f"Course: {course} User: {user}")
+            print(f"User: {user} added -> Course: {course} Section: {section}")
 
 
 client.run(token)
-        
-
-    
-    
-
-
